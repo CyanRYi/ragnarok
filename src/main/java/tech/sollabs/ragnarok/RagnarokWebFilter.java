652f1de3ba1510196855c4c34fd168b825d804a3
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-03-06 20:08:11 +0900

    Change invalid class name : RagnarokWebHandler -> RagnarokWebFilter Add if clause : return 503 status code while server shutting down

--- src/main/java/tech/sollabs/ragnarok/RagnarokWebFilter.java
+++ src/main/java/tech/sollabs/ragnarok/RagnarokWebFilter.java
@@ -1,20 +1,27 @@
 package tech.sollabs.ragnarok;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+import tech.sollabs.ragnarok.configuration.RagnarokConfiguration;
 
-public class RagnarokWebHandler implements WebFilter {
+public class RagnarokWebFilter implements WebFilter {
 
     private RagnarokWatcher ragnarokWatcher;
 
-    public RagnarokWebHandler(RagnarokWatcher ragnarokWatcher) {
+    public RagnarokWebFilter(RagnarokWatcher ragnarokWatcher) {
         this.ragnarokWatcher = ragnarokWatcher;
     }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+
+        if (RagnarokConfiguration.requestedShutdown) {
+            return Mono.error(new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "shut down"));
+        }
 
         ragnarokWatcher.increaseRequestCount();
 
