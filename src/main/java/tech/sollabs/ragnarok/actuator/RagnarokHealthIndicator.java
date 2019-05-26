9321a869c497c4a70e4792b0157f1df81926039f
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-05-26 10:34:48 +0900

    add Comment

--- src/main/java/tech/sollabs/ragnarok/actuator/RagnarokHealthIndicator.java
+++ src/main/java/tech/sollabs/ragnarok/actuator/RagnarokHealthIndicator.java
@@ -9,6 +9,16 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Component;
 import tech.sollabs.ragnarok.RagnarokWatcher;
 
+/**
+ * Show status about tasks managed by RagnarokWatcher
+ * IF RagnarokWatcher bean is not initialized, Health Status is <code>UNKNOWN</code>,
+ * IF Application is now shutting down, Health Status is <code>OUT_OF_SERVICE</code>,
+ * and all other case, Health Status is <code>UP</code>
+ *
+ * @author Cyan Raphael Yi
+ * @since 0.2.0
+ * @see RagnarokWatcher
+ */
 @ConditionalOnClass(ApplicationHealthIndicator.class)
 @Component
 public class RagnarokHealthIndicator implements HealthIndicator {
